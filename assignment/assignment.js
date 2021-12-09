f20d86d0051278239a3ff5e2e39e013bfc49cd27
Christopher Razidlo <cd.razidlo@gmail.com>
2021-12-09 12:01:51 -0600

    added buttons and input

--- assignment/assignment.js
+++ assignment/assignment.js
@@ -3,4 +3,22 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('okay jquery let\'s go');
+    $('#chrisbutton').on( 'click', chrisItUp );
+    $('#otherbutton').on( 'click', yourTextItUp );
+}
+
+function chrisItUp() {
+    $('#headings1to6').children().text('Chris was here');
+    $('img').attr('src', 'chris.jpg' )
+    $('p').text('chris was here');
+}
+
+function yourTextItUp() {
+    $('p').text( getTextInput() );
+}
+
+function getTextInput() {
+    let yourText = $('#yourTextHere').val();
+    $('#yourTextHere').val('');
+    return yourText;
 }
